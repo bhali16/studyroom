7410bc02af80a87a6254bf0760d1a756bb322eeb
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2023-09-20 01:40:27 +0500

    Practice Jinja Templating

--- base/views.py
+++ base/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+rooms = [
+    {'id':1, 'name': "Python"},
+    {'id':3, 'name': "Javascript"},
+    {'id':2, 'name': "Laravel"},
+]
+
 def home(request):
     return render(request, 'home.html')
 
 def room(request):
-    return render(request, 'room.html')
+    context = {'rooms':rooms}
+    return render(request, 'room.html', context)
