0d54f794c8de2ba1845d25c4ee2ec2324b1cd543
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2023-09-20 18:20:03 +0500

    Created Models

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Room
 
 
 rooms = [
@@ -9,8 +10,16 @@ rooms = [
 ]
 
 def home(request):
-    return render(request, 'base/home.html')
-
-def room(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
+
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
     return render(request, 'base/room.html', context)
