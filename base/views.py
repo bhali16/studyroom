4c14d75f32076fe6ea5b5355c80f5e9a97b7d88b
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2023-09-24 13:27:32 +0500

    feat: registerUser method using UserCreationForm

--- base/views.py
+++ base/views.py
@@ -5,15 +5,9 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from . models import Room, Topic
 from . forms import RoomForm
-
-
-rooms = [
-    {'id':1, 'name': "Python"},
-    {'id':3, 'name': "Javascript"},
-    {'id':2, 'name': "Laravel"},
-]
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -81,16 +75,18 @@ def deleteRoom(request,pk):
 
 def loginPage(request):
     
+    page = 'login'
+    
     if request.user.is_authenticated:
         return redirect('home')
     
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
         except:
-            messages.error(request, "User does not exits")
+            messages.error(request, 'User does not exits')
         
         user = authenticate(request, username=username, password=password)
         
@@ -98,11 +94,27 @@ def loginPage(request):
             login(request, user)
             return redirect('home')
         else:
-            messages.error(request, "Username of password does not exits")
+            messages.error(request, 'Username of password does not exits')
     
-    context = {}
+    context = {'page':page}
     return render(request,'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerUser(request):
+    form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration.')
+    context = {'form': form}
+    return render(request, 'base/login_register.html', context)
