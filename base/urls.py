cb366cde2e6a7d4d98e128547e3c839ed569b933
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2023-09-25 09:21:48 +0500

    feat: user profile and delete message route

--- base/urls.py
+++ base/urls.py
@@ -6,9 +6,13 @@ urlpatterns = [
     path('register', views.registerUser, name="register"),
     path('logout', views.logoutUser, name="logout"),
     path('', views.home, name="home"),
+    # path('profile', views.userProfile, name="profile"),
     path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<str:pk>', views.userProfile, name="user-profile"),
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+
 ]
 
